47661506bfdcfdcea1adb8457cc3e00511c38d35
sudosf <sudosprogrammer@gmail.com>
2023-01-05 20:48:26 +0200

    secured exposed passwords

--- register/server/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("SERVER_NAME", "btmyfdxxa6ko8xoeay9i-mysql.services.clever-cloud.com");
-define("USER_NAME", "ui5m1j8vfgyrjrii");
-define("PASSWORD", "JLCyjTqzRAmqV5u41sEo");
-define("DATABASE", "btmyfdxxa6ko8xoeay9i");
-
-?>
--- server/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("SERVER_NAME", "is3-dev.ict.ru.ac.za");
-define("USER_NAME", "G20N8123");
-define("PASSWORD", "tragristy22");
-define("DATABASE", "galacticsystemenforcers");
-
-?>
