911f10a7ab69ebacd92358e31b4c0be75a38fdf1
sudosf <sudosprogrammer@gmail.com>
2023-09-10 02:45:39 +0200

    added config.php file and dev env errors visibility

--- components/head.inc.php
+++ components/head.inc.php
@@ -1,4 +1,9 @@
-<?php session_start(); ?>
+<?php session_start();
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+ ?>
 
 <!-- Main Head for all pages -->
 
---
+++ server/config.php
@@ -0,0 +1,7 @@
+<?php
+define("SERVER_NAME", "bo9iypwwuxqlidby5ghj-mysql.services.clever-cloud.com");
+define("USER_NAME", "utwcg5g3zs4w2z4p");
+define("PASSWORD", "3sa8d5McHb0eUKGveKUv");
+define("DATABASE", "bo9iypwwuxqlidby5ghj");
+
+?>
