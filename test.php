bbceee78f562139b2b2ba4d91bb62ad8bf1fa5b7
sudosf <sudosprogrammer@gmail.com>
2023-09-20 05:33:20 +0200

    added testing page

---
+++ test.php
@@ -0,0 +1,44 @@
+<?php
+	include("components/head.inc.php");
+?>
+
+	<title> Testing </title>
+</head>
+
+<body>
+
+
+	<!-- Start your project here-->
+
+	<header class="container-fluid p-5" style="height: 5%">
+		<!-- Navbar -->
+		<nav class="container navbar navbar-expand navbar-light bg-white topbar fixed-top shadow">
+		</nav>
+	</header>
+
+	<!-- Content Wrapper -->
+	<div id="content-wrapper" class="container-fluid my-5">
+
+		<!-- Main Content -->
+		<div id="content">
+
+
+	
+
+
+
+
+
+
+
+
+
+		</div>
+		<!-- End of Main Content -->
+
+	</div> <!-- End of Content Wrapper -->
+
+	<?php include("portal_components/scripts.inc.php"); ?>
+	<!-- End your project here-->
+</body>
+</html>
